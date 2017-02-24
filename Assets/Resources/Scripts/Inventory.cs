0ca31918c8ec1c267cbd43d192094a1dee8d54dd
Stian Torjussen <stiant92@gmail.com>
2017-02-24 17:10:30 +0100

    inventory class init

--- Assets/Resources/Scripts/Inventory.cs
+++ Assets/Resources/Scripts/Inventory.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour {
+    private List<InventoryItem> inventory;
 
 	// Use this for initialization
 	void Start () {
-		
+        inventory = new List<InventoryItem>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void addItem(InventoryItem item) { inventory.Add(item); }
+    public bool removeItem(InventoryItem item) { return inventory.Remove(item); }
+    public InventoryItem getItemAt(int i) { return i < inventory.Count ? inventory[i] : new InventoryItem(); }
 }
