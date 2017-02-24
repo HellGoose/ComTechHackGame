aee086b71c56608cf01594ee02a36c6331fe4130
Sondretn <sondre_stuff@hotmail.com>
2017-02-24 15:16:07 +0100

    Created a branch for working on the online multiplayer functionality of our game

---
+++ Assets/Movmentscript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movmentscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        float x = 0.0f;
+        float y = 0.0f;
+        float speed = 0.1f;
+        float h = speed * Input.GetAxis("Mouse X");
+        float v = speed * Input.GetAxis("Mouse Y");
+        transform.Translate(h, v, 0);
+
+        if (Input.GetKeyDown("up"))
+        {
+            y = Time.deltaTime * speed;
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            y = -Time.deltaTime * speed;
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            x = -Time.deltaTime * speed;
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            x = Time.deltaTime * speed;
+        }
+        
+        transform.Translate(x , y, 0);
+    }
+}
