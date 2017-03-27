ca7cb5434a0eaf947a0dd74ec8292f875844ad25
Sondretn <sondre_stuff@hotmail.com>
2017-03-27 12:56:49 +0200

    Multiplayer GUI stuff

---
+++ Assets/Multiplayer/NetworkGUIscript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkGUIscript : NetworkBehaviour
+{
+    NetworkIdentity id;
+    Text nameText;
+    Text secretText;
+
+	// Use this for initialization
+	void Start () {
+        id = gameObject.GetComponent<NetworkIdentity>();
+
+        Text[] text = GetComponentsInChildren<Text>();
+
+        for(int i = 0; i < text.Length; i++)
+        {
+            if (text[i].name == "SecretText")
+            {
+                secretText = text[i];
+            }
+            if (text[i].name == "PlayerName")
+            {
+                nameText = text[i];
+            }
+        }
+
+        if(isLocalPlayer)
+        {
+            nameText.text = "eyy lamo" + id.name.ToString();
+            Debug.Log(id.name);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
