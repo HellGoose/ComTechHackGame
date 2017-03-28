9e08941d81640ad8136384cfe88d09a5c371d01a
Anna Berg <Anna Berg>
2017-03-28 02:11:45 +0200

    four players

--- Assets/Resources/Scripts/PlayerUI.cs
+++ Assets/Resources/Scripts/PlayerUI.cs
@@ -8,14 +8,34 @@ public class PlayerUI : MonoBehaviour {
 	//static Sprite player;
     public static int currentNode;
     public static Color color;
+    public int player;
 
     // Use this for initialization
     void Start () {
-        currentNode = 01;
-        color = Color.red;
-		
-		
-	}
+
+        if (player == 1)
+        {
+            currentNode = 01;
+            color = Color.red;
+        }
+        else if (player == 2)
+        {
+            currentNode = 05;
+            color = Color.blue;
+        }
+        else if (player == 3)
+        {
+            currentNode = 21;
+            color = Color.green;
+        }
+        else if (player == 4)
+        {
+            currentNode = 25;
+            color = Color.yellow;
+        }
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
