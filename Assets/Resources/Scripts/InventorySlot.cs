8a3b0fbcfcb6f277c4e12690d5a1716e421b41d5
Stian Torjussen <stiant92@gmail.com>
2017-03-30 21:15:09 +0200

    added a clicklistener for itemSlots

---
+++ Assets/Resources/Scripts/InventorySlot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour, IPointerClickHandler {
+    private Text moduleName;
+    private Text moduleLevel;
+    private Inventory inventory;
+
+    // Use this for initialization
+    void Start () {
+        inventory = GetComponentInParent<Inventory>();
+        moduleName = GetComponentsInChildren<Text>()[0];
+        moduleLevel = GetComponentsInChildren<Text>()[1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+}
