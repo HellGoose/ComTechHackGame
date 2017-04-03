2ee66b3121a2bfef53d21014d65b908ad5932d9b
Sondretn <sondre_stuff@hotmail.com>
2017-04-03 19:49:21 +0200

    Created a  custom multiplayer menu

--- Assets/Resources/Scripts/NetworkGUIscript.cs
+++ Assets/Resources/Scripts/NetworkGUIscript.cs
@@ -4,39 +4,54 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class NetworkGUIscript : NetworkBehaviour
+public class NetworkGUIscript : MonoBehaviour
 {
-    NetworkIdentity id;
-    Text nameText;
-    Text secretText;
+    private HacManNetworkManager networkManager;
+    private Button Host;
+    private Button Join;
+    private InputField playerName;
+    private InputField hostIP;
 
 	// Use this for initialization
 	void Start () {
-        id = gameObject.GetComponent<NetworkIdentity>();
+        networkManager = GameObject.FindObjectOfType<HacManNetworkManager>();
+        Host = GameObject.Find("HostButton").GetComponent<Button>();
+        Join = GameObject.Find("JoinButton").GetComponent<Button>();
+        playerName = GameObject.Find("NameInputField").GetComponent<InputField>();
+        hostIP = GameObject.Find("JoinInputField").GetComponent<InputField>();
 
-        Text[] text = GetComponentsInChildren<Text>();
-
-        for(int i = 0; i < text.Length; i++)
-        {
-            if (text[i].name == "SecretText")
-            {
-                secretText = text[i];
-            }
-            if (text[i].name == "PlayerName")
-            {
-                nameText = text[i];
-            }
-        }
-
-        if(isLocalPlayer)
-        {
-            nameText.text = "eyy lamo" + id.name.ToString();
-            Debug.Log(id.name);
-        }
+        Host.onClick.AddListener(hostOnClick);
+        Join.onClick.AddListener(joinOnClick);
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void hostOnClick() {
+        if(playerName.text != string.Empty)
+        {
+            networkManager.StartHost();   
+        }
+    }
+
+    void joinOnClick() {
+        if (playerName.text != string.Empty && hostIP.text != string.Empty)
+        {
+            
+            string[] ipAndPort = hostIP.text.Split(':');
+            
+            if( ipAndPort.Length == 2)
+            {
+                networkManager.networkAddress = ipAndPort[0];
+                networkManager.networkPort = int.Parse(ipAndPort[1]);
+                networkManager.StartClient();
+            } 
+        }
+    }
+
+    
+    
 }
