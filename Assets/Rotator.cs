0811c3b28432450b85e9e2ed85baf270b452c9a4
kameisler <kameisler@gmail.com>
2017-04-16 19:05:46 +0200

    Waiting screen

---
+++ Assets/Rotator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    
+    public Transform center;
+    public float degreespersec = -65f;
+    private Vector3 v;
+    
+	// Use this for initialization
+	void Start () {
+    
+        v = transform.position - center.position;
+            }
+	
+	// Update is called once per frame
+	void Update () {
+        v = Quaternion.AngleAxis(degreespersec * Time.deltaTime, Vector3.forward) * v;
+        transform.position = center.position + v;
+	}
+    
+}
---
+++ Assets/Waiting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Waiting : MonoBehaviour {
+    public Text text;
+    private int numofletters;
+    private string theText;
+
+	// Use this for initialization
+	void Start () {
+        updatenumofletters();
+        StartCoroutine("animateText");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+    IEnumerator animateText()
+    {
+
+        if (text.text.Length < numofletters + 4)
+        {
+           
+            text.text += ".";
+            yield return new WaitForSeconds(1);
+        }
+        else
+        {
+            text.text = theText;
+        }
+
+        StartCoroutine("animateText");
+    }
+    private void updatenumofletters()
+    {
+        theText = text.text;
+        numofletters = text.text.Length;
+    }
+}
